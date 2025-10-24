986223b64ab77e01d4810ffb8ee98f958c97e44b
Swedka121 <ivan.hryshchuk2011@gmail.com>
2025-10-24 19:08:37 +0300

    feat: add robots.txt and refactor layout for improved SEO and font loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Mulish:ital,wght@0,200..1000;1,200..1000&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        ></link>
+        <meta
+          name="google-site-verification"
+          content="K1Eccn_PYfEMvcl_HpIrRE2h1YKOzaxFT0SZfMnK5L0"
+        />
+      </head>
       <body className="p-0 m-0">
         <SidebarMount defaultOpen={false}>
           <Toaster closeButton={true} position="top-center" />
